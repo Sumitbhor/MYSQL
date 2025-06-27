e6dcfdfa26e619af159acfe63584b2f4f20771e0
Sahil161967 <sahilbajkamble@gmail.com>
2025-06-27 15:27:54 +0530

    disp

--- tflstudentsdb/include/header.h
+++ tflstudentsdb/include/header.h
@@ -2,12 +2,11 @@
 #include<stdlib.h>
 #include <string.h>
 #include<mysql.h>
-<<<<<<< HEAD
-using namespace std;
-void 
-=======
+
+
+
 int addrecord(struct student *ptrstudent, MYSQL *conn);
->>>>>>> 14b8eca0554fb711c492d8d6a22a7cb5c8597393
+
 struct student
 {
     int id ;
